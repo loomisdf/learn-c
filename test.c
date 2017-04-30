d75cf57b433f41641e22acd7bb5a15e8297e86ea
loomisdf <loomisdf@gmail.com>
2017-04-30 15:29:46 -0400

    modify testing file

--- test.c
+++ test.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 struct Car {
 	int fuel_level;
 	int num_doors;
 } car;
+
+int string_length(char* s) {
+	const char *sc;
+	for(sc = s; *sc != '\0'; sc++){}
+
+	return sc - s;
+}
 
 int main(int argc, char* argv[]) {
 	struct Car c1;
@@ -19,10 +27,15 @@ int main(int argc, char* argv[]) {
 	num[0] = 10;
 	num[1] = 20;
 
+	char* str = "hello";
+
 	printf("Fuel level: %d\nNumber of doors: %d\n", c1.fuel_level, c1.num_doors);
 	printf("Fuel level: %d\nNumber of doors: %d\n", c2.fuel_level, c2.num_doors);
 	printf("%d\n", *num);
 	printf("%d\n", num[1]);
+
+	printf("%d\n", string_length(str));
+	printf("%d\n", string_length(""));
 
 	free(num);
 
